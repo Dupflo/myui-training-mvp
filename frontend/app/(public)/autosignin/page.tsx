2cdf7ecc828e19eaca743066e10aa9d7726c462d
Florian Dupuis <floriandupuis1996@gmail.com>
2025-03-08 20:15:31 +0100

    feat: WIP

--- frontend/app/(public)/autosignin/page.tsx
+++ frontend/app/(public)/autosignin/page.tsx
@@ -1,51 +1,11 @@
-"use client"
+import { SearchParams } from "next/dist/server/request/search-params"
+import AutosigninClient from "./autosigninclient"
 
-import { loggedUserFromEmail } from "@/app/(auth)/actions/auth"
-import { useRouter, useSearchParams } from "next/navigation"
-import { Suspense, useEffect } from "react"
-
-export default function Autosignin() {
-  const searchParams = useSearchParams()
-  const emailParams = searchParams.get("email")
-  const router = useRouter()
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      if (emailParams) {
-        await loggedUserFromEmail(emailParams as string)
-        router.push("/")
-      }
-    }
-    fetchUser()
-  }, [emailParams])
-
-  if (searchParams.has("email"))
-    return (
-      <Suspense fallback={<div>Chargement...</div>}>
-        <div className="flex items-center justify-center gap-2 h-screen">
-          <svg
-            className="h-8 w-8 mt-0.5 animate-spin text-black"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-          <h1 className="text-4xl font-semibold">Chargement</h1>
-        </div>
-      </Suspense>
-    )
-  return null
+export default async function AutosigninPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>
+}) {
+  const { email } = await searchParams
+  return <AutosigninClient emailParams={email} />
 }

e3208b7477efc374f27e003b604744398e51e1e1
Florian Dupuis <floriandupuis1996@gmail.com>
2025-03-10 11:53:02 +0100

    feat: WIP

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -22,6 +23,26 @@ export default function AuhtLayout({
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
       <body>{children}</body>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "${process.env.MS_CLARITY_ID}");
+            `}
+      </Script>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
+      ></Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.GTM_ID}');
+                `}
+      </Script>
     </html>
   )
 }
